a2b43345ec5d8e9db5d66662cf18776ae579db1a
Esteban Mandirola <esteban.mandirola@despegar.com>
2020-06-02 21:40:23 -0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "tebi"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation(kotlin("test"))
+    testImplementation("org.assertj:assertj-core:3.16.1")
+
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
