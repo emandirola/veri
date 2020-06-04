78fd09ed9071f5070717c20f0c6b92925ec032d3
Esteban Mandirola <esteban.mandirola@despegar.com>
2020-06-03 21:24:30 -0300

    overdraft in wiring - red

--- src/test/kotlin/tebi/veri/service/MoneyTransferTest.kt
+++ src/test/kotlin/tebi/veri/service/MoneyTransferTest.kt
@@ -1,6 +1,7 @@
 package tebi.veri.service
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.catchThrowable
 import org.junit.Before
 import org.junit.Test
 import tebi.veri.infra.repository.InMemoryClientRepository
@@ -26,5 +27,21 @@ class MoneyTransferTest {
         // then
         assertThat(clientService.balance("francisco")).isEqualTo(0)
         assertThat(clientService.balance("alejandro")).isEqualTo(100)
+    }
+
+    @Test
+    fun `can't transfer if not enough money`() {
+        // given
+        clientService.createClient("francisco")
+        clientService.deposit("francisco", 100)
+        clientService.createClient("alejandro")
+
+        // when
+        val throwable = catchThrowable { clientService.wire("francisco", "alejandro", 200) }
+
+        // then
+        assertThat(throwable).isInstanceOf(IllegalArgumentException::class.java)
+        assertThat(clientService.balance("francisco")).isEqualTo(100)
+        assertThat(clientService.balance("alejandro")).isEqualTo(0)
     }
 }
